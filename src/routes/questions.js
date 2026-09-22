31b0510014306ee0d06bc068d3975778f0c309d0
Santhoshkumar K <165382689+SanthoshKumar020@users.noreply.github.com>
2026-09-22 15:20:04 +0530

    Cleanup old routes

--- src/routes/questions.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generate } = require('../controllers/questionController');
-const { protect } = require('../middleware/auth');
-
-router.post('/generate', protect, generate);
-
-module.exports = router;
